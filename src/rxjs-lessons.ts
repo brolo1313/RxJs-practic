038424f5a4c289bd892d0c555edba0a66dfa5c40
Олександр Радчук <73553177+brolo1313@users.noreply.github.com>
2021-11-25 20:47:32 +0200

    from event next/map

---
+++ src/rxjs-lessons.ts
@@ -0,0 +1,33 @@
+import { debounceTime, Observable, map, distinctUntilChanged, fromEvent } from 'rxjs';
+
+
+
+// const search$ = new Observable<Event>((observer) => {
+//   const search = document.getElementById('search');
+
+//   if (!search) {
+//     observer.error('Element does not exist on the page');
+//     return;
+//   }
+
+//   search!.addEventListener('input', (event) => {
+//     observer.next(event);
+//   });
+// });
+
+const search$: Observable<Event> = fromEvent<Event>(
+  document.getElementById('search'),
+  'input'
+);
+
+
+search$.pipe(
+    map(event => {
+      return (event.target as HTMLInputElement).value;
+    }),
+      debounceTime(500),
+      map(value=>{value.length > 3 ? value : ''} ),
+      distinctUntilChanged()
+    ).subscribe(value => {
+    console.log(value);
+  });
