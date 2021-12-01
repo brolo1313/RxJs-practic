842616d2b03a8a5b55b631523f18204b65df4e39
Олександр Радчук <73553177+brolo1313@users.noreply.github.com>
2021-12-01 17:00:38 +0200

    unsuscribe  moment

--- src/rxjs-lessons.ts
+++ src/rxjs-lessons.ts
@@ -1,17 +1,47 @@
-import {fromEvent, Observable} from 'rxjs';
-import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
+import { fromEvent, Observable } from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  takeUntil,
+} from 'rxjs/operators';
 
 // const search$ = new Observable<Event>(observer => {
 //   const search = document.getElementById('search');
-//
-//   if (!search) {
+//   const stop = document.getElementById('stop');
+
+//   if (!search || !stop) {
 //     observer.error('Element does not exist on the page');
 //     return;
 //   }
-//
-//   search.addEventListener('input', event => {
+
+//   const onSearch = (event: any) => {
+//     console.log(123);
+//     checkSubscription();
 //     observer.next(event);
-//   });
+
+//   };
+
+//   const onStop = (event: any)  => {
+//     checkSubscription();
+//     observer.complete();
+//     clear();
+//   }
+
+//   search.addEventListener('input', onSearch);
+//   stop.addEventListener('click', onStop);
+
+//   const checkSubscription = () => {
+//     if(observer.closed){
+
+//       clear();
+//     }
+//   }
+
+//   const clear = () => {
+//     search.removeEventListener('input', onSearch);
+//     stop.removeEventListener('click', onStop);
+//   }
 // });
 
 const search$: Observable<Event> = fromEvent<Event>(
@@ -19,13 +49,31 @@ const search$: Observable<Event> = fromEvent<Event>(
   'input'
 );
 
-search$.pipe(
-  map(event => {
-    return (event.target as HTMLInputElement).value;
-  }),
-  debounceTime(500),
-  map(value => value.length > 3 ? value : ''),
-  distinctUntilChanged(),
-).subscribe(value => {
-  console.log(value);
-});
+const stop$: Observable<Event> = fromEvent<Event>(
+  document.getElementById('stop') as any as HTMLElement,
+  'click'
+);
+
+const searchSubscription = search$
+  .pipe(
+    map((event) => {
+      return (event.target as HTMLInputElement).value;
+    }),
+    debounceTime(500),
+    map((value) => (value.length > 3 ? value : '')),
+    distinctUntilChanged(),
+    takeUntil(stop$)
+  )
+  .subscribe((value) => {
+    console.log(value);
+  });
+
+// setTimeout(() => {
+//   console.log('unsubscribe');
+//   searchSubscription.unsubscribe();
+// }, 10000)
+
+// const stopSubscription = stop$.subscribe(() => {
+//   searchSubscription.unsubscribe();
+//   stopSubscription.unsubscribe();
+// })
