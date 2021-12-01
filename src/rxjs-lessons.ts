63b9428807c329e261548884581b4b3069cb1326
Олександр Радчук <73553177+brolo1313@users.noreply.github.com>
2021-12-01 15:47:46 +0200

    fixed error

--- src/rxjs-lessons.ts
+++ src/rxjs-lessons.ts
@@ -1,33 +1,31 @@
-import { debounceTime, Observable, map, distinctUntilChanged, fromEvent } from 'rxjs';
+import {fromEvent, Observable} from 'rxjs';
+import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
 
-
-
-// const search$ = new Observable<Event>((observer) => {
+// const search$ = new Observable<Event>(observer => {
 //   const search = document.getElementById('search');
-
+//
 //   if (!search) {
 //     observer.error('Element does not exist on the page');
 //     return;
 //   }
-
-//   search!.addEventListener('input', (event) => {
+//
+//   search.addEventListener('input', event => {
 //     observer.next(event);
 //   });
 // });
 
 const search$: Observable<Event> = fromEvent<Event>(
-  document.getElementById('search'),
+  document.getElementById('search') as any as HTMLElement,
   'input'
 );
 
-
 search$.pipe(
-    map(event => {
-      return (event.target as HTMLInputElement).value;
-    }),
-      debounceTime(500),
-      map(value=>{value.length > 3 ? value : ''} ),
-      distinctUntilChanged()
-    ).subscribe(value => {
-    console.log(value);
-  });
+  map(event => {
+    return (event.target as HTMLInputElement).value;
+  }),
+  debounceTime(500),
+  map(value => value.length > 3 ? value : ''),
+  distinctUntilChanged(),
+).subscribe(value => {
+  console.log(value);
+});
